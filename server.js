c08c133c6a9567850952c09217ec92d053fa18fb
cidiumtester <54253843+cidiumtester@users.noreply.github.com>
2022-07-25 01:28:42 -0600

    Routes update
    Se actualizan las rutas para consultar el perfil de un usuario, crear nuevos posts y mostrar todos los posts

--- server.js
+++ server.js
@@ -14,77 +14,94 @@ app.use(express.json());
 app.use(cors())
 
 
-// Star Wars Characters (DATA)
+// CryptoBlogD (TEST DATA)
 // =============================================================
-var characters = [
+var users = [
   {
     routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
+    correo: "Yoda@jedimaster.com",
+    password: "may4thbewithyou",
+    userName: "cryptoMaster",
+    idioma: "English",
+    moneda: "USD"
   },
   {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
+    routeName: "test",
+    correo: "test@test.com",
+    password: "test",
+    userName: "test",
+    idioma: "English",
+    moneda: "USD"
   }
 ];
+
+var posts = [
+ {
+  userName:"test",
+  postContent:"This is a sample post"
+ },
+ {
+  userName:"test",
+  postContent:"This is a sample post 2"
+ }]
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.send("Welcome to the Star Wars Api!");
+  res.send("Crypto Blog D API");
 });
 
 
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+// Displays all users
+app.get("/profile", function(req, res) {
+  return res.json(users);
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
+// Displays a single user profile, or returns false
+app.get("/profile/:user", function(req, res) {
+  var chosen = req.params.user;
 
   console.log(chosen);
 
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+  for (var i = 0; i < users.length; i++) {
+    if (chosen === users[i].routeName) {
+      return res.json(users[i]);
     }
   }
 
   return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
+// Create New user - takes in JSON input
+app.post("/users", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newcharacter = req.body;
+  var newuser = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  newuser.routeName = newuser.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newcharacter);
+  console.log(newuser);
 
-  characters.push(newcharacter);
+  users.push(newuser);
 
-  res.json(newcharacter);
+  res.json(newuser);
 });
+
+//create new post
+app.post("/newpost",function(req, res){
+ var newpost = req.body;
+ posts.push(newpost);
+ res.json(newpost);
+}); 
+
+// Displays all posts
+app.get("/posts", function(req, res) {
+    return res.json(posts);
+  });
 
 // Starts the server to begin listening
 // =============================================================
